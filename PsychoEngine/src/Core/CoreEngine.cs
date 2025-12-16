bad1d58fcd4917ce34e289e1bde03809972f790a
Roy Soriano <roysr.coding@gmail.com>
2025-12-16 02:30:16 -0600

    Created /Core to match namespaces. Created GlobalUsings.cs

---
+++ PsychoEngine/src/Core/CoreEngine.cs
@@ -0,0 +1,13 @@
+﻿namespace PsychoEngine.Core;
+
+public class CoreEngine : Game
+{
+    public CoreEngine(string windowTitle, int windowWidth, int windowHeight)
+    {
+        GraphicsDeviceManager deviceManager = new(this);
+        deviceManager.PreferredBackBufferWidth = windowWidth;
+        deviceManager.PreferredBackBufferHeight = windowHeight;
+
+        Window.Title = windowTitle;
+    }
+}
