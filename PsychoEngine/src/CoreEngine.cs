86df31fab08b455a668eb4dd4e3b5c3cb4a7be63
Roy Soriano <roysr.coding@gmail.com>
2025-12-15 20:51:36 -0600

    Renamed PsychoEngine class to CoreEngine. Moved source files to /src. Added parameters to CoreEngine class.

---
+++ PsychoEngine/src/CoreEngine.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace PsychoEngine.Core;
+
+public class CoreEngine : Game
+{
+    public CoreEngine(string windowTitle, int windowWidth, int windowHeight)
+    {
+        GraphicsDeviceManager deviceManager = new(this);
+        deviceManager.PreferredBackBufferWidth = windowWidth;
+        deviceManager.PreferredBackBufferHeight = windowHeight;
+        
+        Window.Title = windowTitle;
+    }
+}
