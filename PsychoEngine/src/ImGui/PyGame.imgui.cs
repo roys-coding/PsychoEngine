8fb43315a02c2c36feda74aa5975698218ebd45d
roys-coding <roysr.coding@gmail.com>
2026-01-12 14:01:54 -0600

    Moved ImGui-related members to a separate file.
    Made PyGame partial and moved ImGui members to a separate .imgui file.
    Cleanup and reformatting.

---
+++ PsychoEngine/src/ImGui/PyGame.imgui.cs
@@ -0,0 +1,101 @@
+﻿using Hexa.NET.ImGui;
+using Hexa.NET.ImPlot;
+
+namespace PsychoEngine;
+
+public partial class PyGame
+{
+    private void InitializeImGui()
+    {
+        ImGuiManager.OnLayout += ImGuiOnLayout;
+    }
+
+    private void ImGuiOnLayout(object? sender, EventArgs eventArgs)
+    {
+        const ImGuiDockNodeFlags dockFlags =
+            ImGuiDockNodeFlags.PassthruCentralNode | ImGuiDockNodeFlags.NoDockingOverCentralNode;
+
+        ImGui.DockSpaceOverViewport(dockFlags);
+
+        ImGui.ShowDemoWindow();
+        ImPlot.ShowDemoWindow();
+
+        if (ImGui.IsKeyDown(ImGuiKey.F) && ImGui.IsKeyDown(ImGuiKey.ModShift))
+        {
+            // 60 fps.
+            TargetElapsedTime = TimeSpan.FromTicks(166667);
+        }
+
+        if (ImGui.IsKeyDown(ImGuiKey.Escape))
+        {
+            Exit();
+        }
+
+        bool gameWindow = ImGui.Begin("Game");
+
+        if (!gameWindow)
+        {
+            ImGui.End();
+            return;
+        }
+
+        bool exitPressed = ImGui.Button("Exit");
+        ImGui.SameLine();
+        ImGui.TextDisabled("(ESC)");
+
+        if (exitPressed)
+        {
+            Exit();
+        }
+
+        bool timesHeader = ImGui.CollapsingHeader("Times");
+
+        if (timesHeader)
+        {
+            float targetElapsedMs = (float)TargetElapsedTime.TotalMilliseconds;
+            bool  elapsedChanged  = ImGui.SliderFloat("TargetMs", ref targetElapsedMs, 1f, 500.0f);
+
+            if (elapsedChanged)
+            {
+                TargetElapsedTime = TimeSpan.FromMilliseconds(targetElapsedMs);
+            }
+
+            float fps        = 1000f / targetElapsedMs;
+            bool  fpsChanged = ImGui.SliderFloat("TargetFPS", ref fps, 2f, 1000f);
+
+            if (fpsChanged)
+            {
+                TargetElapsedTime = TimeSpan.FromMilliseconds(1000f / fps);
+            }
+
+            float sleepMs        = (float)InactiveSleepTime.TotalMilliseconds;
+            bool  sleepMsChanged = ImGui.SliderFloat("InactiveMs", ref sleepMs, 0f, 1000f);
+
+            if (sleepMsChanged)
+            {
+                InactiveSleepTime = TimeSpan.FromMilliseconds(sleepMs);
+            }
+
+            ImGui.TextDisabled("Press Shift+F to reset to 60 FPS!");
+
+            bool isFixed        = IsFixedTimeStep;
+            bool isFixedChanged = ImGui.Checkbox("FixedTime", ref isFixed);
+
+            if (isFixedChanged)
+            {
+                IsFixedTimeStep = isFixed;
+            }
+
+            bool vsync        = _deviceManager.SynchronizeWithVerticalRetrace;
+            bool vsyncChanged = ImGui.Checkbox("VSync", ref vsync);
+
+            if (vsyncChanged)
+            {
+                _deviceManager.SynchronizeWithVerticalRetrace = vsync;
+                _deviceManager.ApplyChanges();
+            }
+        }
+
+        ImGui.End();
+    }
+}
--- PsychoEngine/src/PyGame.cs
+++ PsychoEngine/src/PyGame.cs
@@ -1,16 +1,13 @@
 using System.Diagnostics.CodeAnalysis;
-using Hexa.NET.ImGui;
-using Hexa.NET.ImPlot;
 using PsychoEngine.Input;
 
 namespace PsychoEngine;
 
-public class PyGame : Game
+public partial class PyGame : Game
 {
     private readonly GraphicsDeviceManager _deviceManager;
 
-    [AllowNull]
-    public static PyGame Instance { get; private set; }
+    [AllowNull] public static PyGame Instance { get; private set; }
 
     internal ImGuiManager ImGuiManager { get; }
 
@@ -34,74 +31,7 @@ public class PyGame : Game
 
         ImGuiManager = new ImGuiManager(this);
 
-        ImGuiManager.OnLayout += ImGuiOnLayout;
-    }
-
-    private void ImGuiOnLayout(object? sender, EventArgs eventArgs)
-    {
-        const ImGuiDockNodeFlags dockFlags =
-            ImGuiDockNodeFlags.PassthruCentralNode | ImGuiDockNodeFlags.NoDockingOverCentralNode;
-
-        ImGui.DockSpaceOverViewport(dockFlags);
-
-        ImGui.ShowDemoWindow();
-        ImPlot.ShowDemoWindow();
-
-        if (ImGui.IsKeyDown(ImGuiKey.F) && ImGui.IsKeyDown(ImGuiKey.ModShift))
-        {
-            // 60 fps.
-            TargetElapsedTime = TimeSpan.FromTicks(166667);
-        }
-
-        bool gameWindow = ImGui.Begin("Game");
-
-        if (!gameWindow)
-        {
-            ImGui.End();
-            return;
-        }
-
-        bool exitPressed = ImGui.Button("Exit");
-        ImGui.SameLine();
-        ImGui.TextDisabled("(ESC)");
-        if (exitPressed || ImGui.IsKeyDown(ImGuiKey.Escape)) Exit();
-
-        bool timesHeader = ImGui.CollapsingHeader("Times");
-
-        if (timesHeader)
-        {
-            float targetElapsedMs = (float)TargetElapsedTime.TotalMilliseconds;
-            bool  elapsedChanged  = ImGui.SliderFloat("TargetMs", ref targetElapsedMs, 1f, 500.0f);
-
-            if (elapsedChanged) TargetElapsedTime = TimeSpan.FromMilliseconds(targetElapsedMs);
-
-            float fps        = 1000f / targetElapsedMs;
-            bool  fpsChanged = ImGui.SliderFloat("TargetFPS", ref fps, 2f, 1000f);
-
-            if (fpsChanged) TargetElapsedTime = TimeSpan.FromMilliseconds(1000f / fps);
-
-            float sleepMs        = (float)InactiveSleepTime.TotalMilliseconds;
-            bool  sleepMsChanged = ImGui.SliderFloat("InactiveMs", ref sleepMs, 0f, 1000f);
-
-            if (sleepMsChanged) InactiveSleepTime = TimeSpan.FromMilliseconds(sleepMs);
-
-            ImGui.TextDisabled("Press Shift+F to reset to 60 FPS!");
-
-            bool isFixed                        = IsFixedTimeStep;
-            bool isFixedChanged                 = ImGui.Checkbox("FixedTime", ref isFixed);
-            if (isFixedChanged) IsFixedTimeStep = isFixed;
-
-            bool vsync        = _deviceManager.SynchronizeWithVerticalRetrace;
-            bool vsyncChanged = ImGui.Checkbox("VSync", ref vsync);
-
-            if (vsyncChanged)
-            {
-                _deviceManager.SynchronizeWithVerticalRetrace = vsync;
-                _deviceManager.ApplyChanges();
-            }
-        }
-
-        ImGui.End();
+        InitializeImGui();
     }
 
     protected override void Initialize()
