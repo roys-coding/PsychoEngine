b328e51f4fb4a9940621dafe040508e15cc92e46
roys-coding <roysr.coding@gmail.com>
2026-01-19 14:48:41 -0600

    Implemented basic API

--- PsychoEngine/src/Console/PyConsole.cs
+++ PsychoEngine/src/Console/PyConsole.cs
@@ -2,4 +2,30 @@
 
 public static class PyConsole
 {
+    private readonly struct LogMessage
+    {
+        public string Message { get; }
+        public LogSeverity Severity { get; }
+        
+        public LogMessage(string message, LogSeverity severity)
+        {
+            Message       = message;
+            Severity = severity;
+        }
+    }
+    
+    private static readonly List<LogMessage> LoggedMessages;
+    
+    static PyConsole()
+    {
+        LoggedMessages = [];
+    }
+    
+    public static void Log(string message, LogSeverity severity) { }
+    public static void LogDebug(string message) { }
+    public static void LogInfo(string message) { }
+    public static void LogSuccess(string message) { }
+    public static void LogWarning(string message) { }
+    public static void LogError(string message) { }
+    public static void LogFatal(string message) { }
 }
