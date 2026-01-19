8f9818107482f376aeb1cc141c6c39147fca5108
roys-coding <roysr.coding@gmail.com>
2026-01-19 14:54:01 -0600

    Added implementation for all Log methods.

--- PsychoEngine/src/Console/PyConsole.cs
+++ PsychoEngine/src/Console/PyConsole.cs
@@ -4,14 +4,14 @@ public static class PyConsole
 {
     private readonly struct LogMessage
     {
-        public string Message { get; }
+        public string      Message  { get; }
         public LogSeverity Severity { get; }
-        public string Category { get; }
-        
+        public string      Category { get; }
+
         public LogMessage(string message, LogSeverity severity, string category)
         {
-            Message       = message;
-            Severity      = severity;
+            Message  = message;
+            Severity = severity;
             Category = category;
         }
     }
@@ -22,12 +22,50 @@ public static class PyConsole
     {
         LoggedMessages = [];
     }
-    
-    public static void Log(string message, LogSeverity severity = LogSeverity.Info, string category = "") { }
-    public static void LogDebug(string message, string category = "") { }
-    public static void LogInfo(string message, string category = "") { }
-    public static void LogSuccess(string message, string category = "") { }
-    public static void LogWarning(string message, string category = "") { }
-    public static void LogError(string message, string category = "") { }
-    public static void LogFatal(string message, string category = "") { }
+
+    public static bool Clear()
+    {
+        if (LoggedMessages.Count == 0)
+        {
+            return false;
+        }
+        
+        LoggedMessages.Clear();
+        return true;
+    }
+
+    public static void Log(string message, LogSeverity severity = LogSeverity.Info, string category = "")
+    {
+        LoggedMessages.Add(new LogMessage(message, severity, category));
+    }
+
+    public static void LogDebug(string message, string category = "")
+    {
+        Log(message, LogSeverity.Debug, category);
+    }
+
+    public static void LogInfo(string message, string category = "")
+    {
+        Log(message, LogSeverity.Info, category);
+    }
+
+    public static void LogSuccess(string message, string category = "")
+    {
+        Log(message, LogSeverity.Success, category);
+    }
+
+    public static void LogWarning(string message, string category = "")
+    {
+        Log(message, LogSeverity.Warning, category);
+    }
+
+    public static void LogError(string message, string category = "")
+    {
+        Log(message, LogSeverity.Error, category);
+    }
+
+    public static void LogFatal(string message, string category = "")
+    {
+        Log(message, LogSeverity.Fatal, category);
+    }
 }
